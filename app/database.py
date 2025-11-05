b64d28d90b54c6b5ebbce144228e9f0eec63ec6b
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-05 13:12:39 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -7,3 +7,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
+def create_db():
+    Base.metadata.create_all(bind=engine)
