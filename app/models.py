cd18ad315571f046803a8fcf267f1689d3455ca3
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-05 13:34:28 +0100

    Add Post model and relationship with User model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,20 @@
-from sqlalchemy import Column, Integer, String
-from app.database import Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
+from sqlalchemy.orm import relationship
+from .database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+
+    posts = relationship("Post", back_populates="author", cascade="all, delete")
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    author_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    author = relationship("User", back_populates="posts")
