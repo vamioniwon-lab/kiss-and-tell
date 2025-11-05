4d1a8d9b539ac547b7ec540989e26ad11da053d8
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-05 20:16:51 +0100

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,4 +1,17 @@
-from .database import SessionLocal
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
@@ -6,3 +19,6 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# Create tables
+init_db()
