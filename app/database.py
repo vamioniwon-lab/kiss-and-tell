4aeeb58d3e00c81e15c04833e201db922d34c027
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-05 10:36:22 +0100

    Reorganize import statements in database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.settings import settings
+from sqlalchemy.orm import sessionmaker, declarative_base
+from .settings import settings
 
 engine = create_engine(settings.DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
