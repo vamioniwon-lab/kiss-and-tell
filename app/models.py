6cace39c5d369143d9c3c47268d2d9a40422534c
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-05 18:25:54 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,20 +1,9 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, func
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 from .database import Base
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    password_hash = Column(String, nullable=False)
-
-    posts = relationship("Post", back_populates="author", cascade="all, delete")
-
-class Post(Base):
-    __tablename__ = "posts"
-    id = Column(Integer, primary_key=True, index=True)
-    content = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    author_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
-
-    author = relationship("User", back_populates="posts")
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
