ba6737f3f32d211542cdf8150a08debb549c40ca
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-05 13:15:50 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,9 @@
 from sqlalchemy import Column, Integer, String
-from .database import Base
+from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    phone = Column(String, unique=True, index=True)
     password = Column(String)
-    display_name = Column(String)
