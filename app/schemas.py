8be88bf9e314c6ac5d4db705d5f6b275535d1cad
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-05 17:17:42 +0100

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -6,5 +6,17 @@ class SignupRequest(BaseModel):
     password: str
 
 class LoginRequest(BaseModel):
-    username: str
+    email: str
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+
+    class Config:
+        orm_mode = True
