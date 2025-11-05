476bd11605155f6afb3c3d6375193de8651f8f6c
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-05 20:13:54 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,12 @@
 from sqlalchemy import Column, Integer, String
-from .database import Base
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}
+
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
