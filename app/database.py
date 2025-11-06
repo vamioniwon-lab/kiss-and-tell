d35f8e3c9a84ee4acff7d4117e8270fb3b6b4721
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 11:57:43 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,13 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ✅ Correct writable path for Render
-DATABASE_URL = "sqlite:////data/kissandtell.db"
+DATABASE_URL = "postgresql://kiss_and_tell_db_user:qzuhGUE1flC6ok37YR1zmjyQALdR86EQ@dpg-d455o81r0fns73dhoma0-a/kiss_and_tell_db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
