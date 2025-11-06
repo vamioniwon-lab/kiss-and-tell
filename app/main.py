f7628050c3c38ecb6fcfbce7161b34c8840bd45e
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 10:02:34 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from .database import Base, engine
 from .auth import router as auth_router
 
-# create tables once at startup
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Kiss & Tell API")
 
-# mount routers
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
 @app.get("/")
