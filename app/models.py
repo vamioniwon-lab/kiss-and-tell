783fc995084db0444ccbd142efe61d4949181a46
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 08:34:47 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,8 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
-from .db import Base
+from .db import SessionLocal
 
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String(255), unique=True, index=True, nullable=False)
-    password = Column(String(255), nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
