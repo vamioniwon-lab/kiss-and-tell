845fed6ecb85cb5f5da591d0401362ad0c0d782e
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 09:44:22 +0100

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,24 +1,10 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from .models import Base
+from .database import SessionLocal
+from sqlalchemy.orm import Session
+from typing import Generator
 
-DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-
-def get_db():
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-# Create tables
-init_db()
