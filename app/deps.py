ec3306400bd1fb02e7888f4da99723ac311eb438
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 15:47:12 +0100

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,10 +1,20 @@
-from .database import SessionLocal
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
 from sqlalchemy.orm import Session
-from typing import Generator
+from app.database import get_db
+from app.models import User
 
-def get_db() -> Generator[Session, None, None]:
-    db = SessionLocal()
+SECRET = "TEST_SECRET"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     try:
-        yield db
-    finally:
-        db.close()
+        payload = jwt.decode(token, SECRET, algorithms=["HS256"])
+        user_id: int = payload.get("id")
+        if not user_id:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        user = db.query(User).filter(User.id == user_id).first()
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
