a57e1c940266977f2be44074021a71176c0b1d6e
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 09:42:25 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "sqlite:///./kissandtell.db"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},  # needed for SQLite in threads
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
