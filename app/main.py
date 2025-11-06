8ad1dec63ed39e15c6ecb76cc41c9cbea529a4bd
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 13:59:34 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from .auth import router as auth_router
-from .database import create_tables
+from app.database import Base, engine
+from app.auth import router as auth_router
+from app.models import *
+
+# ✅ Create DB tables if they don't exist
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-@app.on_event("startup")
-def on_startup():
-    create_tables()
 
 app.include_router(auth_router, prefix="/auth")
