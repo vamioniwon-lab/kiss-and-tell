054b3abb0f87e7b9baa6318efc23511a5e421b37
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 12:56:05 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 DATABASE_URL = "postgresql://kiss_and_tell_db_user:qzuhGUE1flC6ok37YR1zmjyQALdR86EQ@dpg-d455o81r0fns73dhoma0-a/kiss_and_tell_db"
 
@@ -8,3 +9,8 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+from .models import Base
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
