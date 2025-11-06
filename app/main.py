81bace478d24d052517525c42fd7f1c2847c06f1
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 15:48:58 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
-from app.database import Base, engine
+from app.database import create_tables
 from app.auth import router as auth_router
-from app.models import *
-
-# ✅ Create DB tables if they don't exist
-Base.metadata.create_all(bind=engine)
+from app.confession import router as confession_router
 
 app = FastAPI()
 
-app.include_router(auth_router, prefix="/auth")
+create_tables()
+
+app.include_router(auth_router)
+app.include_router(confession_router)
+
+@app.get("/")
+def root():
+    return {"status": "running"}
