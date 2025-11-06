f0b9f2491ef8091f68299a0e3b3658f27683471b
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-06 15:45:35 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from datetime import datetime
+from sqlalchemy.orm import relationship
 from .database import Base
 
 class User(Base):
@@ -8,4 +9,16 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+class Confession(Base):
+    __tablename__ = "confessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    likes = Column(Integer, default=0)
+    comments = Column(Integer, default=0)
+
+    user = relationship("User", backref="confessions")
