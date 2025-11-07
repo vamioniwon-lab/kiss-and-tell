fdfc02b4543163a5044ad58d607f129840fb57a0
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 21:42:43 +0100

    Update deps.py

--- app/deps.py
+++ app/deps.py
@@ -1,19 +1,9 @@
-from fastapi import Depends, HTTPException
-from jose import jwt, JWTError
-from sqlalchemy.orm import Session
-from .database import get_db
-from .settings import settings
-from .models import User
+from typing import Generator
+from app.database import SessionLocal
 
-def get_current_user(token: str, db: Session = Depends(get_db)):
+def get_db() -> Generator:
+    db = SessionLocal()
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-        email = payload.get("sub")
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Invalid token")
-
-    user = db.query(User).filter(User.email == email).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    return user
+        yield db
+    finally:
+        db.close()
