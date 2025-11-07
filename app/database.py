515f037b0cc45f155736ca7e3dfecbd413ad55ea
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 15:55:02 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,17 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-# Example:
-# DATABASE_URL = "postgresql+psycopg://user:pass@host:port/db"
-
-from os import getenv
-DATABASE_URL = getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
