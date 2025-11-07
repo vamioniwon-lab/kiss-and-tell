53aab864485f950edc063985eef4da56e22514bc
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 22:50:23 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,15 @@ from app.database import Base, engine
 from app.auth import router as auth_router
 from app.reset_db import router as reset_router
 
-# Force drop + recreate tables
-Base.metadata.drop_all(bind=engine)
+# Create DB tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(reset_router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Kiss & Tell API Running ✅"}
