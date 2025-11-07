06358c5d5eb35b7e413a0b04d0f207520d074374
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 17:43:50 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,8 @@
 from fastapi import FastAPI
-from .database import Base, engine
-from .auth import router as auth_router
-from .confession import router as conf_router
+from app.database import engine, Base
+from app.auth import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
-app.include_router(conf_router, prefix="/confessions", tags=["confessions"])
-
-
-@app.get("/")
-def root():
-    return {"message": "Kiss & Tell API running ✅"}
+app.include_router(auth_router)
