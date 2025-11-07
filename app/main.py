ee9776f252145dfbe23a2f16a5e86d36e02e2644
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 16:55:27 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,31 +1,16 @@
-# app/main.py
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .auth import router as auth_router
-from .confession import router as confession_router
+from .confession import router as conf_router
 
-app = FastAPI(
-    title="Kiss & Tell API",
-    version="1.0.0"
-)
-
-# Create DB tables
 Base.metadata.create_all(bind=engine)
 
-# CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = FastAPI()
 
-# Routes
-app.include_router(auth_router, prefix="/auth", tags=["Auth"])
-app.include_router(confession_router, prefix="/confession", tags=["Confession"])
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(conf_router, prefix="/confessions", tags=["confessions"])
 
-@app.get("/", tags=["Default"])
-def home():
-    return {"message": "Kiss & Tell API Running ✅"}
+
+@app.get("/")
+def root():
+    return {"message": "Kiss & Tell API running ✅"}
