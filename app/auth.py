a3cf65bac3ed165097e670150e52763fd947e4d3
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 20:51:02 +0100

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_ctx = CryptContext(schemes=["argon2"])
 
 class UserCreate(BaseModel):
     email: str
@@ -17,7 +17,9 @@ class UserCreate(BaseModel):
 def signup(user: UserCreate, db: Session = Depends(get_db)):
     hashed = pwd_ctx.hash(user.password)
     new_user = User(email=user.email, hashed_password=hashed)
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+
     return {"message": "User created"}
