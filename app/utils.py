937061c359ac12c7e1b480abb861ec678995b3e2
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 09:52:50 +0100

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,19 @@
+from datetime import datetime, timedelta
+from jose import jwt
 from passlib.context import CryptContext
+from .settings import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = settings.SECRET_KEY
 
-def hash_password(password: str) -> str:
+def hash_password(password: str):
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(password: str, hashed: str):
+    return pwd_context.verify(password, hashed)
+
+def create_access_token(data: dict, expires_minutes: int):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expires_minutes)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
