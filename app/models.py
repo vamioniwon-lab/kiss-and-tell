bbdb68edf0065837e82ec661f979c4615ab4f838
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 17:46:19 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text, func
-from .database import Base
+from sqlalchemy import Column, Integer, String
+from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
@@ -7,11 +7,3 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-
-class Confession(Base):
-    __tablename__ = "confessions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    body = Column(Text)
-    created_at = Column(DateTime, server_default=func.now())
