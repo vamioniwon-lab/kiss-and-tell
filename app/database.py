22e650f3ff8ce66228794350139599a15f5ce296
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-07 18:25:14 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# Use DATABASE_URL from Render env
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
