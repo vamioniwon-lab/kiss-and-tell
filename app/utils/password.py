67d6bd26f7a417e3c8d4a7a6f29f60a361c05373
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-08 23:26:55 +0100

    Create password.py

---
+++ app/utils/password.py
@@ -0,0 +1,6 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
