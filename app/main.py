f422f953d4e684e1f0bc8078cce608f896ba95db
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-08 22:13:06 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.auth import router as auth_router
-from app.reset_db import router as reset_router
 
-# Create DB tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 app.include_router(auth_router)
-app.include_router(reset_router)
-
 
 @app.get("/")
 def root():
-    return {"message": "Kiss & Tell API Running ✅"}
+    return {"status": "ok"}
