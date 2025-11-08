e6f52f248fd44c08cb97ab79d7e412f785ea0882
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-08 22:41:32 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,22 +1,8 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-
-    confessions = relationship("Confession", back_populates="owner")
-
-
-class Confession(Base):
-    __tablename__ = "confessions"
-
-    id = Column(Integer, primary_key=True, index=True)
-    content = Column(String, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
-
-    owner = relationship("User", back_populates="confessions")
