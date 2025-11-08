60d51cffbc9d418b70d8c67c4879c0cfc12d6205
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-09 00:11:29 +0100

    Update password.py

--- app/utils/password.py
+++ app/utils/password.py
@@ -3,7 +3,9 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
+    """Hash a plain password."""
     return pwd_context.hash(password)
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str):
+    """Verify a plain password against its hashed version."""
     return pwd_context.verify(plain_password, hashed_password)
