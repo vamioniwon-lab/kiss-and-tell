648b3d0f6ea066b0bb1ed57935e24b335f008ed8
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-08 23:24:11 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
