5dea6e241eeed6fd10614eaa19ccda610c865755
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-08 22:44:15 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,9 @@ from app.database import Base, engine
 from app.auth import router as auth_router
 from app.reset_db import router as reset_router
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)  # create tables on startup
 
 app = FastAPI()
-
-# Routes
 app.include_router(auth_router)
 app.include_router(reset_router)
 
