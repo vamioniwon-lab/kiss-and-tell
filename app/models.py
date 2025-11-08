394faeed90bc992bb06c471de23febfa53845b06
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-09 00:25:14 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,4 +6,4 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
+    password = Column(String, nullable=False)
