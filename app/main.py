367e8733445751eee2b253f6c29f304ca1650e88
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-09 00:59:38 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
-from app.database import engine, Base
 from app.auth import router as auth_router
 from app.maintenance import router as maintenance_router
+from app.database import Base, engine
 
-# Create all tables
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# ROUTES
-app.include_router(auth_router)
+app.include_router(auth_router, prefix="/auth")
 app.include_router(maintenance_router)
-
-@app.get("/")
-def root():
-    return {"message": "Kiss & Tell API is live"}
