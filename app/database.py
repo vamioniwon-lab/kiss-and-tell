6a28c496e2453e9159c71ba0e5a2172b0b9f8157
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-08 23:16:03 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # Render env var, e.g. postgres://...
-# SQLAlchemy needs postgresql+pg8000:// scheme to use pg8000
-DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+pg8000://", 1)
+DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+pg8000://")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
