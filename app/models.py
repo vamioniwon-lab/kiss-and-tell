e5751d1f51f21a24f0c25cea4fdef6b8cc2b5c93
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-09 01:21:13 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,8 +1,25 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
 from app.database import Base
+
 
 class User(Base):
     __tablename__ = "users"
-    id       = Column(Integer, primary_key=True, index=True)
-    email    = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)  # <- this is what DB is missing
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    confessions = relationship("Confession", back_populates="owner")
+
+
+class Confession(Base):
+    __tablename__ = "confessions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(Text, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="confessions")
