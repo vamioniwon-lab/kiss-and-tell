1d7f39a65ec9f91f2c466b0a3824d7aaa938e015
vamioniwon-lab <vamioniwon@gmail.com>
2025-11-09 01:15:48 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.settings import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
-# Fix URL prefix for SQLAlchemy
-DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+pg8000://")
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
